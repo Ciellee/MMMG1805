426f947a4f2823c2213b3a2ba1c859ff89b64711
Ciellee <33077072+Ciellee@users.noreply.github.com>
2018-09-03 17:19:05 +0800

    Rotation script updated
    still incomplete

--- MMMG_Assignment/Assets/Script/Rotation.cs
+++ MMMG_Assignment/Assets/Script/Rotation.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour {
+public class Rotation : MonoBehaviour
+{
+    public int rotationSpeed = 20;
+    public Quaternion direction;
+    float smooth = 5.0f;
+    float tiltAngle = 90.0f;
+    public Quaternion target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            target = Quaternion.Euler(0, 90, 0);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
+        //// Smoothly tilts a transform towards a target rotation.
+        //float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle;
+        //float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        //Quaternion target = Quaternion.Euler(tiltAroundX, tiltAroundY, 0);
+
+        // Dampen towards the target rotation
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
 }
