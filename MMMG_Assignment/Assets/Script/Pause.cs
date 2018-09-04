ff7f520b23b3683638244e8d0c95b7cd46e0e9e1
GongJW0520 <33077763+GongJW0520@users.noreply.github.com>
2018-09-05 07:00:55 +0800

    Pause Button
    Pause button with resume and back to main menu with bgm and sound effect

---
+++ MMMG_Assignment/Assets/Script/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseButton, pausePanel, BTMain;
+
+    public void Start()
+    {
+        OnUnPause();
+    }
+
+	public void OnPause()
+    {
+        pausePanel.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void OnUnPause()
+    {
+        pausePanel.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
