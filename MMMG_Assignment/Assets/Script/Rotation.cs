226359fb4d13d4f61a41436e329b203ec6499e1e
Ciellee <33077072+Ciellee@users.noreply.github.com>
2018-09-04 15:31:03 +0800

    Update script
    - able to rotate smoothly based on mouse click

--- MMMG_Assignment/Assets/Script/Rotation.cs
+++ MMMG_Assignment/Assets/Script/Rotation.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    public int rotationSpeed = 20;
-    public Quaternion direction;
-    float smooth = 5.0f;
-    float tiltAngle = 90.0f;
+    public int rotationSpeed = 10;
+    //float smooth = 5.0f;
+    //float tiltAngle = 90.0f;
     public Quaternion target;
+    public int yRotation;
+
+    private void OnMouseDown()
+    {
+        yRotation += 90; //importatn
+        target = Quaternion.Euler(0, yRotation, 0); //important
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
+
+    }
+
+    //void TouchCube()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hit;
+
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        if (hit.transform.name == "Cube")
+    //        {
+    //            yRotation += 90; //importatn
+    //            target = Quaternion.Euler(0, yRotation, 0); //important
+
+    //        }
+    //        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
+    //        Debug.Log("Cube hit");
+    //    }
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed); //important
+    //}
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            target = Quaternion.Euler(0, 90, 0);
-        }
 
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
+        //TouchCube();
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    target = Quaternion.Euler(0, , 0);
+        //}
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
+        //target = transform.rotation;
+
         //// Smoothly tilts a transform towards a target rotation.
         //float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle;
         //float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
@@ -28,3 +60,4 @@ public class Rotation : MonoBehaviour
         //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     }
 }
+
