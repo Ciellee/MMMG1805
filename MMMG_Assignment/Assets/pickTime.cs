d7afd8a6b11552a99c92efe42ded4d670c9107cd
GongJW0520 <33077763+GongJW0520@users.noreply.github.com>
2018-09-05 13:59:24 +0800

    add tag
    add a new tag

--- MMMG_Assignment/Assets/pickTime.cs
+++ MMMG_Assignment/Assets/pickTime.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+
 public class pickTime : MonoBehaviour {
     public GameObject timePick;
-    public Text bonusTime;
+    public Text time;
+    public int bonus = 10;
     public void OnTriggerEnter()
     {
         timePick.SetActive(false);
-
+        time.text = ""+ bonus + int.Parse(time.text);
     }
 
 }
